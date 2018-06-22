9a565bf45cfd76c2572aa16c84ebd5e87b08bc2f
Kenneth Bolivar Castro <keboca@gmail.com>
2018-06-21 22:13:32 -0600

    Agregar archivos de proyecto.

---
+++ WebMVC-BI-Client/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebMVC_BI_Client.Startup))]
+namespace WebMVC_BI_Client
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
